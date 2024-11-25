b1b0b5fcb478ed6bba69626dfd8d96721bd638e3
djennad <49979434+djennad@users.noreply.github.com>
2024-11-25 22:44:12 +0100

    test
    desc test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "179108892"
+    serviceWorkerVersion: "2951871053"
   }
 });
